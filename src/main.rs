eb405312b16e52636d3639e180a7ade05ae31274
dennie-seth <dennie.seth@hotmail.com>
2025-11-29 11:12:39 +0100

    upd: Ctrl-C, SIGTERM, SIGHUB supported

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod config;
 mod tests;
 
 use std::io::BufRead;
-use std::sync::mpsc;
 use std::thread;
+use tokio::signal;
 use crate::config::parse_config;
 use crate::net::server::start_server;
 
@@ -22,31 +22,70 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     // region: server launch
     let (_, stop_word) = start_server(config)?;
-    let (sender, receiver) = mpsc::channel::<String>();
     thread::spawn(move || {
         let mut lock = std::io::stdin().lock();
         loop {
             let mut line = String::new();
             if lock.read_line(&mut line).is_err() { break; }
-            if sender.send(line).is_err() { break; }
         }
     });
     // endregion
     
     // region: main app loop
-    loop {
-        match receiver.try_recv() {
-            Ok(message) => {
-                assert_ne!(message.trim(), "", "No input!");
-                if message.eq_ignore_ascii_case("q\r\n") {
-                    break;
+    {
+        let stop_word = stop_word.clone();
+        tokio::spawn(async move {
+            let reason = wait_for_shutdown().await;
+            match reason {
+                ShutdownReason::CtrlC => {
+                    println!("Shutting down...");
+                    stop_word.notify();
+                }
+                #[cfg(unix)]
+                ShutdownReason::SigTerm => {
+                    println!("SigTerm received, shutting down...");
+                    stop_word.notify();
+                }
+                #[cfg(unix)]
+                ShutdownReason::SigHup => {
+                    println!("SigHup received, not implemented");
                 }
             }
-            Err(mpsc::TryRecvError::Empty) => {}
-            Err(mpsc::TryRecvError::Disconnected) => { break; }
-        }
+        });
     }
-    stop_word.notify();
     // endregion
     Ok(())
+}
+#[derive(Debug)]
+enum ShutdownReason {
+    CtrlC,
+    #[cfg(unix)]
+    SigTerm,
+    #[cfg(unix)]
+    SigHup,
+}
+async fn wait_for_shutdown() -> ShutdownReason {
+    #[cfg(unix)]
+    {
+        use tokio::signal::unix::{signal, SignalKind};
+        let mut sigterm = signal(SignalKind::terminate()).unwrap();
+        let mut sighup = signal(SignalKind::hangup()).unwrap();
+
+        tokio::select! {
+            _ = signal::ctrl_c() => {
+                return ShutdownReason::CtrlC
+            }
+            _ = sigterm.recv() => {
+                return ShutdownReason::SigTerm
+            }
+            _ = sighup.recv() => {
+                return ShutdownReason::SigHup
+            }
+        }
+    }
+    #[cfg(windows)]
+    {
+        signal::ctrl_c().await.unwrap();
+        ShutdownReason::CtrlC
+    }
 }
